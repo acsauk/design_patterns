f4b555d0f2762a5c4906e2fbf3e4fcf329794ad9
Alex Saunders <acsauk@gmail.com>
2017-07-05 17:14:05 +0100

    adds TyeRotation decorator, again implementing the same interfact as the cores service

--- php/decorator/mechanic.php
+++ php/decorator/mechanic.php
@@ -24,5 +24,19 @@
     }
   }
 
-  echo (new OilChange(new BasicInspection()))->getCost();
+  class TyreRotation implements CarService {
+    protected $carService;
+
+    function __construct(CarService $carService)
+    {
+      $this->carService = $carService;
+    }
+
+    public function getCost()
+    {
+      return 25 + $this->carService->getCost();
+    }
+  }
+
+  echo (new TyreRotation(new OilChange(new BasicInspection())))->getCost();
 ?>
