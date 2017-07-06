cae50fae1748df68dd5e8f21526647019aa5bac1
Alex Saunders <acsauk@gmail.com>
2017-07-06 11:44:22 +0100

    adds eReaderAdapter to allow eReaderInterface to work let Kindle or other eReaders communicate with Person via BookInterface

--- php/adapter/index.php
+++ php/adapter/index.php
@@ -3,9 +3,9 @@
 require 'vendor/autoload.php';
 
 use Acme\Book;
-use Acme\BookInterface;
 use Acme\Kindle;
-use Acme\KindleAdapter;
+use Acme\BookInterface;
+use Acme\eReaderAdapter;
 
 class Person {
   public function read(BookInterface $book)
@@ -16,6 +16,6 @@ class Person {
 }
 
 (new Person)->read(new Book);
-(new Person)->read(new KindleAdapter(new Kindle));
+(new Person)->read(new eReaderAdapter(new Kindle));
 
 ?>
