c11efad58a93731f04684366bbcf34800f831fa7
Alex Saunders <acsauk@gmail.com>
2017-07-06 10:35:52 +0100

    extracts interface from Book class - updates Book class to implement interface and related syntax in index.php

--- php/adapter/index.php
+++ php/adapter/index.php
@@ -3,15 +3,16 @@
 require 'vendor/autoload.php';
 
 use Acme\Book;
+use Acme\BookInterface;
 
 class Person {
-  public function read($book)
+  public function read(BookInterface $book)
   {
     $book->open();
     $book->turnPage();
   }
 }
 
-(new Person)->read(new Book)
+(new Person)->read(new Book);
 
 ?>
